172840f3839df6abe85858a4d4a3d4223616edba
nason <nason@9e129b38-c2c0-4eec-9301-dc54fda404de>
2014-01-27 12:46:23 +0000

    limit units up to 96 (97 and 99 are used for .lhe files)
    git-svn-id: svn://powhegbox.mib.infn.it/trunk/POWHEG-BOX-V2@2621 9e129b38-c2c0-4eec-9301-dc54fda404de

--- newunit.f
+++ newunit.f
@@ -3,7 +3,9 @@
       integer iun
       logical ok
       integer j
-      do j=10,99
+c units 97 and 99 are used for lhe files;
+c keep reserved.
+      do j=10,96
          inquire(unit=j,opened=ok)
          if(.not.ok) then
             iun=j
