ec7dec7f1a7a10c8a16eb2a3387217b9d596eceb
oleari <oleari@9e129b38-c2c0-4eec-9301-dc54fda404de>
2020-08-26 23:45:12 +0000

    added printed line
    git-svn-id: svn://powhegbox.mib.infn.it/trunk/POWHEG-BOX-V2@3784 9e129b38-c2c0-4eec-9301-dc54fda404de

--- bbinit.f
+++ bbinit.f
@@ -1571,6 +1571,7 @@ c     call newunit(iunit)
       write(iunit,'(a)') '      integer nentries,iret'
       write(iunit,'(a)') '      integer equivto(nentries)'
       write(iunit,'(a)') '      real * 8 equivcoef(nentries)'
+      write(iunit,'(a)') '      write(*,*) "Reading table of equivalent amplitudes for '//flag//'"'
       do k=1,2
          if(k==1) then
             write(iunit,'(a)') '      equivto=(/'
